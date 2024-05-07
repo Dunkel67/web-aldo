e22ccf1e2811715ab1dedfbfa5eef07c4c545028
Dunkel67 <93346406+Dunkel67@users.noreply.github.com>
2024-05-07 22:16:46 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
   createHashRouter,
   RouterProvider
 } from 'react-router-dom';
+import App from './App';
+import './index.css';
 import Game from './Views/Game';
 import Test from './Views/Test';
 
@@ -23,3 +25,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+export default App;
