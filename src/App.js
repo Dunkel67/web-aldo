02b3c996cfa310eccac46a12bdfdda32f7804069
Dunkel67 <93346406+Dunkel67@users.noreply.github.com>
2024-05-07 23:12:15 +0200

    changes on route to charge de page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Game from './Views/Game';
-import Test from './Views/Test';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Test/>} />
-        <Route path="/game" element={<Game/>} />
-      </Routes>
-    </Router>
+    <HashRouter>
+          <Routes>
+            <Route path="/" element={<Game/>} />
+          </Routes>
+    </HashRouter>
   );
 }
 
